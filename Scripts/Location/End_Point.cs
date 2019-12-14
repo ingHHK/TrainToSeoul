45c47f3335d43f1160f1912b4142339177bfd655
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:12:39 +0900

    Add files via upload

---
+++ Scripts/Location/End_Point.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CompleteProject;
+
+public class End_Point : MonoBehaviour
+{
+    GameObject player;
+    SerumUI serumUI;
+    private GameController gameController;
+    bool playerInRange = false;
+
+    private int LevelNum;
+
+    void Start(){
+        LevelNum = SceneManager.GetActiveScene().buildIndex;
+        player = GameObject.FindGameObjectWithTag ("Player");
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        serumUI = GameObject.FindGameObjectWithTag("SerumUI").GetComponent<SerumUI>();
+    }
+        // Update is called once per frame
+    void Update(){
+        if(playerInRange && (gameController.timerCount == 6) && (serumUI.currentFill == 3)){
+            Debug.Log("Load Next Episode");
+            LevelNum++;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(LevelNum);
+        }
+    }
+    
+    void OnTriggerEnter (Collider other){
+        // If the entering collider is the player...
+        if(other.gameObject.tag == "Player"){
+        // ... the player is in range.
+            playerInRange = true;
+        }
+    }
+}
---
+++ Scripts/Location/Start_Point.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Point : MonoBehaviour
+{
+    GameObject zombie;
+    GameObject player;
+    bool zombieInRange = false;
+    CompleteProject.PlayerHealth hp;
+
+    void Start(){
+        zombie = GameObject.FindGameObjectWithTag ("Zombie");
+        player = GameObject.FindGameObjectWithTag ("Player");
+        hp = player.GetComponent<CompleteProject.PlayerHealth>();
+    }
+    
+    void Update(){
+        if(zombieInRange){
+            hp.TakeDamage(hp.currentHealth);
+        }
+    }
+    
+    void OnTriggerEnter (Collider other){
+        // If the entering collider is the zombie...
+        if(other.gameObject.tag == "Zombie"){
+        // ... the zombie is in range.
+            zombieInRange = true;
+        }
+    }
+}
