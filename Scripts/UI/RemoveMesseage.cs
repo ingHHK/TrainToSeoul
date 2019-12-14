796298a182038dcae8493e18100ffb416aeac328
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:23:20 +0900

    Add files via upload

---
+++ Scripts/UI/RemoveMesseage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemoveMesseage : MonoBehaviour
+{
+
+    public GameObject DayMesseage;
+    public GameObject NightMesseage;
+
+    public void GetDayOK(){
+        Cursor.visible = false;                     //마우스 커서가 보이지 않게 함
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1.0f;
+        DayMesseage.SetActive(false);
+    }
+
+    public void GetNightOK(){
+        Cursor.visible = false;                     //마우스 커서가 보이지 않게 함
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1.0f;
+        NightMesseage.SetActive(false);
+    }
+}
---
+++ Scripts/UI/RemoveUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemoveUI : MonoBehaviour
+{
+
+    public GameObject HPUI;
+    public GameObject SerumUI;
+    public GameObject BinoUI;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(BinoUI.activeSelf == true){
+            StartCoroutine(DestroyBinoUI(2));
+        }
+
+        if(SerumUI.activeSelf == true){
+            StartCoroutine(DestroySerumUI(2));
+        }
+
+        if(HPUI.activeSelf == true){
+            StartCoroutine(DestroyHPUI(2));
+        }
+    }
+
+    IEnumerator DestroyHPUI(float delalyTime){                                   //안내메시지 ON/OFF
+        yield return new WaitForSeconds(delalyTime);
+        HPUI.SetActive(false);
+    }
+
+    IEnumerator DestroySerumUI(float delalyTime){                                   //안내메시지 ON/OFF
+        yield return new WaitForSeconds(delalyTime);
+        SerumUI.SetActive(false);
+    }
+
+    IEnumerator DestroyBinoUI(float delalyTime){                                   //안내메시지 ON/OFF
+        yield return new WaitForSeconds(delalyTime);
+        BinoUI.SetActive(false);
+    }
+}
