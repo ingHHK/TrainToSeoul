72ba56f7f1c0387db26b2c614dfc8ab4b8838b6f
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:22:18 +0900

    Add files via upload

---
+++ Scripts/Video/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    VideoPlayer vPlayer;
+    int index;
+
+    // Start is called before the first frame update
+    void Awake(){
+        vPlayer = GetComponent<VideoPlayer>();
+        vPlayer.loopPointReached += OnMovieFinished;
+        index = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void OnMovieFinished(VideoPlayer player){
+        player.Stop();
+        
+        if(index < 11)
+            SceneManager.LoadScene(++index);
+        else
+            SceneManager.LoadScene(0);
+    }
+}
