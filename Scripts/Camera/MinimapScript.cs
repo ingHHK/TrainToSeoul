dac086f111981b28858759aa4a1b1a4d9e6a89ce
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:09:21 +0900

    Add files via upload

---
+++ Scripts/Camera/MinimapScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    Transform player;
+    // Start is called before the first frame update
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+        transform.position = new Vector3(player.position.x, 0, player.position.z);
+    }
+}
