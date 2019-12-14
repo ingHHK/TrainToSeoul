9354b0c67bd0dfb8ec273f34ecec3fe8fe8e1e6b
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:07:31 +0900

    Add files via upload

---
+++ Scripts/BGM/BGMController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+    public AudioClip Night_BGM;
+    public AudioClip Day_BGM;
+
+    private AudioSource audio;
+
+    public void night(){
+        audio.clip = Night_BGM;
+        audio.Play();
+    }
+
+    public void day(){
+        audio.clip = Day_BGM;
+        audio.Play();
+    }
+
+    public void stop(){
+        audio.Stop();
+    }
+    public void pause(){
+        audio.Pause();
+    }
+
+    public void resume(){
+        audio.Play();
+    }
+
+    void Awake(){
+        audio = GetComponent<AudioSource>();
+        audio.clip = Day_BGM;
+    }
+}
