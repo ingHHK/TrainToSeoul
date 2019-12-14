7f53b5b05c1bee6adc3ced3f4e945dab90cea2a6
woosteelz <48507475+woosteelz@users.noreply.github.com>
2019-12-14 22:15:12 +0900

    Add files via upload

---
+++ Scripts/StartScene/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler
+{
+    AudioSource reload;
+    public bool mouseClicked = false;
+    int index;
+
+    // Start is called before the first frame update
+    void Start(){
+        reload = GetComponent<AudioSource>();
+        index = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        Debug.Log("Reolad!!");
+        reload.Play();
+    }
+
+    public void NextScene(){
+        if(index < 11)
+            SceneManager.LoadScene(++index);
+        else
+            SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+    public void PrevScene(){
+        SceneManager.LoadScene(--index);
+        Time.timeScale = 1f;
+    }
+}
---
+++ Scripts/StartScene/StartScenePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScenePlayer : MonoBehaviour
+{
+    Animator anim;
+    private float horizontalMove;
+    private float verticalMove;
+    private float rotate;
+    private Transform tr;
+    public float moveSpeed = 6f;
+    public float rotSpeed = 80f;
+
+    // Start is called before the first frame update
+    void Start(){
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        horizontalMove = Input.GetAxisRaw ("Horizontal");
+        verticalMove = Input.GetAxisRaw ("Vertical");
+        rotate=Input.GetAxis("Mouse X");
+        AnimationUpdate();
+    }
+
+    void AnimationUpdate(){
+        if(horizontalMove == 0 && verticalMove == 0){
+            anim.SetBool("IsWalking", true);
+        }
+        else{
+            anim.SetBool("IsWalking", false);
+        }
+    }
+}
